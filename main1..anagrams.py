f54ca0dfa48e8c06c25b658f3a6feed04822bae8
Nazeef Danladi Adamu <nerzeefei@gmail.com>
2022-05-26 21:47:34 -1200

    anagrams

---
+++ main1..anagrams.py
@@ -0,0 +1,15 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    print(sorted(word), sorted(anagram))
+    if sorted(word) == sorted(anagram):
+        print(sorted(word),sorted(anagram))
+        return True
+    return False
+
+
+print(find_anagram("hello", "bellow"))
